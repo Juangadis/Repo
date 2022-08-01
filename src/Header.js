5ace9030312b329b2d13cb561dec09a5b9cb3f94
Juangadis <juanestevecorena98@gmail.com>
2022-08-01 17:06:39 -0300

    Repaso

--- src/Header.js
+++ src/Header.js
@@ -3,8 +3,8 @@ import Nav from './Nav'
 
 const Header = () => {
     return (
-        <header id='layout-header'>
-            <h1 className='header__title'>Mi E-Commerce</h1>
+        <header id='layout-header' className='Jorgito'>
+            <h1 className='header__title'>Mi E-Commerce je</h1>
             <span class="material-icons">shopping_cart</span>
             <Nav/>
         </header>
